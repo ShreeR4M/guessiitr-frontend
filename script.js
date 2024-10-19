89c67ee76225513882af095cc3cbadde612825a2
jshishimaru <jbsss785@gmail.com>
2024-10-19 06:03:40 +0530

    add marker and guess button

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function onMapClick(e) {
-	alert("You clicked the map at " + e.latlng);
+	marker.setLatLng(e.latlng);
 }
 
-var map = L.map('map').setView([29.854263, 77.888000], 15);
+var map = L.map('map').setView([29.754263, 77.888000], 15);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([29.854263, 77.888000]).addTo(map);
 
 map.on('click', onMapClick);
